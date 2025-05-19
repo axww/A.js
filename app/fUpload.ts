f48e92f117fc0295229b5e51eb9e0dd16d9018a0
axww <114227169+awwr@users.noreply.github.com>
2025-05-19 22:28:04 +0900

    Image to ImgBB

--- app/fUpload.ts
+++ app/fUpload.ts
@@ -1,18 +1,14 @@
 import { Context } from "hono";
-import { fileTypeFromBlob } from 'file-type';
 
 export async function fUpload(a: Context) {
     const time = Math.floor(Date.now() / 1000);
-    const file = await a.req.blob();
-    if (!(await fileTypeFromBlob(file))?.mime.startsWith('image/')) {
-        return a.text('Unsupported Media Type', 415);
-    }
+    const blob = await a.req.blob();
     const form = new FormData();
-    form.append('reqtype', 'fileupload');
-    form.append('fileToUpload', file, time + '');
-    const response = await fetch('https://catbox.moe/user/api.php', { method: 'POST', body: form });
+    form.append('key', '02b0ac824fd4f83e161f51d767dd9b25');
+    form.append('image', blob, time + '');
+    const response = await fetch('https://api.imgbb.com/1/upload', { method: 'POST', body: form });
     if (response.ok) {
-        return a.text((await response.text()).split('/').at(-1) ?? '')
+        return a.json(await response.json())
     } else {
         return a.text(await response.text(), 500)
     }
--- const/a.js
+++ const/a.js
@@ -128,12 +128,12 @@ function upload() {
                     if (!response.ok) {
                         throw new Error('[' + response.status + '] ' + await response.text());
                     }
-                    return await response.text();
+                    return await response.json();
                 })
-                .then(fid => {
-                    if (!fid) { return false; }
+                .then(json => {
+                    if (!Object.keys(json).length || !json.success) { alert('Request Error'); return false; }
                     const range = quill.getSelection();
-                    quill.insertEmbed(range.index, 'image', 'https://i0.wp.com/files.catbox.moe/' + fid + '?w=1920');
+                    quill.insertEmbed(range.index, 'image', 'https://i0.wp.com/' + json.data.url.substring(8) + '?w=1920');
                     quill.setSelection(range.index + 1);
                 })
                 .catch(error => {
