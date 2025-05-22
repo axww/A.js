29bf59aa3ebd37254496cd189bb915da610fbe50
axww <114227169+awwr@users.noreply.github.com>
2025-05-23 02:38:21 +0900

    Quote optimise

--- src/message.tsx
+++ src/message.tsx
@@ -32,7 +32,7 @@ export async function _mList(a: Context) {
         ))
         .leftJoin(Post, eq(Post.pid, Message.pid))
         .leftJoin(User, eq(User.uid, Post.uid))
-        .leftJoin(QuotePost, eq(QuotePost.pid, sql`CASE WHEN ${Post.quote_pid} = 0 THEN ${Post.tid} ELSE ${Post.quote_pid} END`))
+        .leftJoin(QuotePost, eq(QuotePost.pid, Post.quote_pid))
         .orderBy(desc(Message.uid), desc(Message.type), desc(Message.pid))
         .limit(10)
     await Promise.all(data.map(async function (row) {
