b5001ed6b261f7741d8bc79accd60ed88f386219
axww <114227169+awwr@users.noreply.github.com>
2025-07-15 20:44:54 +0900

    Re-use catbox

--- src/file.tsx
+++ src/file.tsx
@@ -1,13 +1,17 @@
 import { Context } from "hono";
+import { fileTypeFromBlob } from 'file-type';
 
 export async function fUpload(a: Context) {
-    const blob = await a.req.blob();
+    const file = await a.req.blob();
+    if (!(await fileTypeFromBlob(file))?.mime.startsWith('image/')) {
+        return a.text('Unsupported Media Type', 415);
+    }
     const form = new FormData();
-    form.append('key', a.env.ImgBBKey);
-    form.append('image', blob, a.get('time').toString());
-    const response = await fetch('https://api.imgbb.com/1/upload', { method: 'POST', body: form });
+    form.append('reqtype', 'fileupload');
+    form.append('fileToUpload', file, a.get('time').toString());
+    const response = await fetch('https://catbox.moe/user/api.php', { method: 'POST', body: form });
     if (response.ok) {
-        return a.json((await response.json()) as JSON)
+        return a.text((await response.text()).split('/').at(-1) ?? '')
     } else {
         return a.text(await response.text(), 500)
     }
